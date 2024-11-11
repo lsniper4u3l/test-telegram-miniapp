766c856c4e5aaa4a1c7b93e7ab47e469755df245
Phu <phuaeoof@gmail.com>
2024-11-11 11:52:24 +0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import { useEffect } from "react";
+import Script from "next/script";
+import "../styles/globals.css"; // ใช้ไฟล์ CSS หลักของโปรเจค (ถ้ามี)
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // เริ่มต้น Web App SDK ของ Telegram
+    if (typeof window !== "undefined") {
+      const WebApp = window.Telegram.WebApp;
+      WebApp.ready();
+    }
+  }, []);
+
+  return (
+    <>
+      {/* โหลด SDK ของ Telegram */}
+      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
